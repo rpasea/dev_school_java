0ed3a4e9697de8f9e981910f41ea5e084a9af008
Iulian Craciun <ic@iuliancraciun.com>
2018-04-18 08:34:49 +0300

    Realizat serializarea

--- session1/jsonparser/src/main/java/com/example/serialization/json/MapSerializer.java
+++ session1/jsonparser/src/main/java/com/example/serialization/json/MapSerializer.java
@@ -10,27 +10,61 @@ import java.util.Map;
 class MapSerializer {
     public StringBuilder serialize(Map<String, Object> jsonObject) throws SerializationException {
         // TODO
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        int i = 0;
+        for (Map.Entry<String, Object> entry : jsonObject.entrySet()){
+            i++;
+            if (i > 1 ){
+                sb.append(",");
+            }
+            sb.append("\"").append(entry.getKey()).append("\"").append(":");
+            sb.append(serialize(entry.getValue()));
+        }
+        sb.append("}");
+        return sb;
     }
 
     public StringBuilder serialize(Collection<Object> collection) throws SerializationException {
         // TODO
-        return null;
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int dimensiune = collection.size();
+        if (dimensiune > 1){
+            sb.append("[");
+        }
+        for (Object obj : collection){
+            i++;
+            if (i > 1){
+                sb.append(",");
+            }
+            sb.append(serialize(obj));
+        }
+        if (dimensiune > 1){
+            sb.append("]");
+        }
+        return sb;
     }
 
     public StringBuilder serialize(String string) {
         // TODO
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("\"").append(string).append("\"");
+        return sb;
     }
 
     public StringBuilder serialize(Number number) {
         // TODO
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append(number);
+        return sb;
     }
 
     public StringBuilder serialize(Boolean bool) {
         // TODO
-        return null;
+        StringBuilder sb = new StringBuilder();
+        sb.append(bool);
+        return sb;
     }
 
     public StringBuilder serialize(Object object) throws SerializationException {
