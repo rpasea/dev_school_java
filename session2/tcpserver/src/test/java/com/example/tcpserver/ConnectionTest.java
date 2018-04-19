ab3da1760abbebfb05a93553eb52159a95cc1117
Radu Pasea <radu.pasea@ing.ro>
2018-04-19 14:17:12 +0300

    add session2

---
+++ session2/tcpserver/src/test/java/com/example/tcpserver/ConnectionTest.java
@@ -0,0 +1,53 @@
+package com.example.tcpserver;
+
+import com.example.tcpserver.codec.Codec;
+import com.example.tcpserver.codec.CodecPipeline;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.nio.ByteBuffer;
+import java.util.Collections;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+// Not the runner
+@RunWith(MockitoJUnitRunner.class)
+public class ConnectionTest {
+    private static final String REQUEST = "mock_request";
+    private static final ByteBuffer SERIALIZED_REQUEST = ByteBuffer.wrap(new byte[]{-12, -34, 0, 34, 126});
+
+    // You can control the behavior of Mock objects and inspect their interactions
+    @Mock
+    private TcpServer server;
+    @Mock
+    private CodecPipeline codecPipeline;
+    @Mock
+    private MessageHandler handler;
+    @Mock
+    private Codec codec;
+
+    // The Object under test
+    @InjectMocks
+    private Connection connection;
+
+    @Before
+    public void setup() {
+        when(codecPipeline.getReadOrder()).thenReturn(Collections.singletonList(codec).iterator());
+        /*
+         * You should use Mockito to correctly wire the mocks so you can implement your unit test
+         */
+    }
+
+    @Test
+    public void whenReceivingMessageShouldDecodeWithCodecsCallHandlerAndSendResponse() {
+        connection.received(SERIALIZED_REQUEST);
+
+        verify(server).send(connection, SERIALIZED_REQUEST);
+    }
+
+}
