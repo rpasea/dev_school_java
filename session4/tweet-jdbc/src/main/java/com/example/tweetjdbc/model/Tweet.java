ff1509905acbd2983cd668e4dd1da641ea4bb1f6
Radu Pasea <radu.pasea@ing.ro>
2018-04-26 15:15:29 +0300

    add session4 code

---
+++ session4/tweet-jdbc/src/main/java/com/example/tweetjdbc/model/Tweet.java
@@ -0,0 +1,9 @@
+package com.example.tweetjdbc.model;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+public class Tweet {
+    // TODO
+}
