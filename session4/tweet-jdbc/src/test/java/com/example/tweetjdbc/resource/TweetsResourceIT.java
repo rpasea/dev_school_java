09ca6f330d3bd1b8b7b7415a968fb4d999e1f578
Iuian Craciun <ic@iuliancraciun.com>
2018-05-03 19:44:22 +0300

    tst

--- session4/tweet-jdbc/src/test/java/com/example/tweetjdbc/resource/TweetsResourceIT.java
+++ session4/tweet-jdbc/src/test/java/com/example/tweetjdbc/resource/TweetsResourceIT.java
@@ -1,8 +1,11 @@
 package com.example.tweetjdbc.resource;
 
+import com.example.tweetjdbc.model.Tweet;
 import com.example.tweetjdbc.repository.TweetDAO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -10,6 +13,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+
+import java.util.List;
 import java.nio.charset.Charset;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -26,14 +34,22 @@ public class TweetsResourceIT {
 
     @MockBean
     private TweetDAO tweetDAO;
+    @Autowired
+    private ObjectMapper mapper;
 
     @Test
     public void shouldRetrieveTweets() throws Exception {
         // you should also check the result itself, not only the code
+        List<Tweet> tweets = Arrays.asList(new Tweet(12, "text", 12, LocalDateTime.now()));
+
+
+
+        Mockito.when(tweetDAO.getTweets()).thenReturn(tweets);
         mockMvc.perform(
                 get("/tweets")
                         .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print()).andExpect(status().is(200));
+                .andDo(print()).andExpect(status().is(200))
+        .andExpect(content().json(mapper.writeValueAsString(tweets)));
     }
 
     @Test
